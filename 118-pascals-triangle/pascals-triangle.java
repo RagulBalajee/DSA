9baed48ad8ac621e4103c67e0bdcb3bf6b7d61bd
Ragul Balajee GK <148249750+RagulBalajee@users.noreply.github.com>
2025-09-13 23:14:45 +0530

    Time: 1 ms (86.62%) | Memory: 42.1 MB (60.38%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        int n = numRows;
+        int[][] arr = new int[n][n];  // 2D array to store Pascal's triangle
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            List<Integer> rowList = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {   // first or last element = 1
+                    arr[i][j] = 1;
+                } else {
+                    arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+                }
+                rowList.add(arr[i][j]);
+            }
+            result.add(rowList);
+        }
+
+        return result;
+    }
+}
