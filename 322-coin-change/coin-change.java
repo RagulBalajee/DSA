3cb80fe2531451bfae92c4a13bc8ac13a0ad8003
Ragul Balajee GK <148249750+RagulBalajee@users.noreply.github.com>
2025-12-12 10:18:11 +0530

    Time: 20 ms (36.24%) | Memory: 47 MB (19.08%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,23 +1,20 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         if(amount == 0) return 0;
-        //if(amount==coins[0]) return 1;
-        int n=coins.length;
-        int a = amount+1;
-        int[][] dp = new int[n+1][amount+1];
-        for(int i=0;i<=n;i++)
+        int[][] dp = new int[coins.length+1][amount+1];
+        for(int i=0;i<=coins.length;i++)
         {
             for(int j=0;j<=amount;j++)
             {
-                dp[i][j]=a;
+                dp[i][j]=amount+1;
             }
         }
-        for(int i=0;i<=n;i++)
+        for(int i=0;i<=coins.length;i++)
         {
             dp[i][0]=0;
         }
         
-        for(int i=1;i<=n;i++){
+        for(int i=1;i<=coins.length;i++){
             for(int j=1;j<=amount;j++){
                 if(j>=coins[i-1]) 
                 dp[i][j]= Math.min(dp[i-1][j],1+dp[i][j-coins[i-1]]);
@@ -25,7 +22,6 @@ class Solution {
                 dp[i][j]= dp[i-1][j];
             }
         }
-         int result = dp[n][amount];
-        return result == amount+1 ? -1 : result;
+        return dp[coins.length][amount] == amount+1 ? -1 : dp[coins.length][amount];
     }
 }
